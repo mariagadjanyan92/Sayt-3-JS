05ebc1590434d28f9d79e00a487fb55915ed8a79
mariagadjanyan92 <marina.agadjanyan92@mail.ru>
2023-03-31 15:21:35 +0400

    first commit

---
+++ JS Sayt.js
@@ -0,0 +1,465 @@
+let arr=[
+    {
+        name:"“Armenia” white dry wine", 
+        persent: "Alc 55% 0.75l",
+        url: "wino1.png",
+       oldprice:"3.59",
+        sale:"10%",
+        icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOoPLBr6dN2ha1EVjelvUgn5UdGzVcUbwqkA&usqp=CAU",
+        article1:"Article",
+        article2:"000123",
+        country1:"Country",
+        country2:"Armenia",
+        region1:"Region",
+        region2:"Armenia",
+         manufacturer1:"Manufacturer",
+        manufacturer2:"Areni",
+        color:"color red",
+        type:"Type dry",
+        Volume:"Valume 750ml",
+        Alkohol:"Alcohol 55%",
+        price:"5,99",
+        Description:"Description",
+        text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a venenatis ex. Praesent ut diam sit amet ipsum hendrerit lacinia nec ut turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed sollicitudin lacus sed quam laoreet, et elementum diam varius. Etiam eu placerat libero.Suspendisse a venenatis ex. Praesent ut diam sit amet ipsum hendrerit lacinia nec ut turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed sollicitudin lacus sed quam laoreet, et elementum diam varius. Etiam eu placerat libero.  ",
+      
+   
+    },
+    {
+        name:"“Armenia” rose sweet wine", 
+        persent: "Alc 55% 0.75l",
+        url: "wino2.png",
+       oldprice:" ",
+        sale:"",
+        line:"",
+        icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOoPLBr6dN2ha1EVjelvUgn5UdGzVcUbwqkA&usqp=CAU",
+        article1:"Article",
+        article2:"000123",
+        country1:"Country",
+        country2:"Armenia",
+        region1:"Region",
+        region2:"Armenia",
+         manufacturer1:"Manufacturer",
+        manufacturer2:"Areni",
+        color:"color red",
+        type:"Type dry",
+        Volume:"Valume 750ml",
+        Alkohol:"Alcohol 55%",
+        price:"9,99",
+        Description:"Description",
+        text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a venenatis ex. Praesent ut diam sit amet ipsum hendrerit lacinia nec ut turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed sollicitudin lacus sed quam laoreet, et elementum diam varius. Etiam eu placerat libero.Suspendisse a venenatis ex. Praesent ut diam sit amet ipsum hendrerit lacinia nec ut turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed sollicitudin lacus sed quam laoreet, et elementum diam varius. Etiam eu placerat libero.  ",
+     
+    },
+    {
+        name:"“Koor” red sweet wine", 
+        persent: "Alc 55% 0.75l",
+        url: "wino3.png",
+       oldprice:"9,55 ",
+        sale:"10%",
+        icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOoPLBr6dN2ha1EVjelvUgn5UdGzVcUbwqkA&usqp=CAU",
+        article1:"Article",
+        article2:"000123",
+        country1:"Country",
+        country2:"Armenia",
+        region1:"Region",
+        region2:"Armenia",
+         manufacturer1:"Manufacturer",
+        manufacturer2:"Areni",
+        color:"color red",
+        type:"Type dry",
+        Volume:"Valume 750ml",
+        Alkohol:"Alcohol 55%",
+        price:"7,00",
+        Description:"Description",
+        text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a venenatis ex. Praesent ut diam sit amet ipsum hendrerit lacinia nec ut turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed sollicitudin lacus sed quam laoreet, et elementum diam varius. Etiam eu placerat libero.Suspendisse a venenatis ex. Praesent ut diam sit amet ipsum hendrerit lacinia nec ut turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed sollicitudin lacus sed quam laoreet, et elementum diam varius. Etiam eu placerat libero.  ",
+   
+    },
+    {
+        name:"“Armenia” pomegrante", 
+        persent: "Alc 55% 0.75l",
+        url: "wino4.png",
+       oldprice:"9,55 ",
+        sale:"10%",
+        icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOoPLBr6dN2ha1EVjelvUgn5UdGzVcUbwqkA&usqp=CAU",
+        article1:"Article",
+    article2:"000123",
+    country1:"Country",
+    country2:"Armenia",
+    region1:"Region",
+    region2:"Armenia",
+     manufacturer1:"Manufacturer",
+    manufacturer2:"Areni",
+    color:"color red",
+    type:"Type dry",
+    Volume:"Valume 750ml",
+    Alkohol:"Alcohol 55%",
+    price:"7,69",
+    Description:"Description",
+    text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a venenatis ex. Praesent ut diam sit amet ipsum hendrerit lacinia nec ut turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed sollicitudin lacus sed quam laoreet, et elementum diam varius. Etiam eu placerat libero.Suspendisse a venenatis ex. Praesent ut diam sit amet ipsum hendrerit lacinia nec ut turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed sollicitudin lacus sed quam laoreet, et elementum diam varius. Etiam eu placerat libero.  ",
+    
+    }
+    
+]
+let arr2=[
+    {
+    name:"“Karas” red wine", 
+    persent: "Alc 55% 0.75l",
+    url: "wino5.png",
+    oldprice:"3.59",
+     sale:"10%",
+     icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOoPLBr6dN2ha1EVjelvUgn5UdGzVcUbwqkA&usqp=CAU",
+     article1:"Article",
+     article2:"000123",
+     country1:"Country",
+     country2:"Armenia",
+     region1:"Region",
+     region2:"Armenia",
+      manufacturer1:"Manufacturer",
+     manufacturer2:"Areni",
+     color:"color red",
+     type:"Type dry",
+     Volume:"Valume 750ml",
+     Alkohol:"Alcohol 55%",
+     price:"5,99",
+     Description:"Description",
+     text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a venenatis ex. Praesent ut diam sit amet ipsum hendrerit lacinia nec ut turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed sollicitudin lacus sed quam laoreet, et elementum diam varius. Etiam eu placerat libero.Suspendisse a venenatis ex. Praesent ut diam sit amet ipsum hendrerit lacinia nec ut turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed sollicitudin lacus sed quam laoreet, et elementum diam varius. Etiam eu placerat libero.  ",
+    
+    },
+    {
+        name:"“Zorah” white dry wine", 
+        persent: "Alc 55% 0.75l",
+        url: "wino6.png",
+       oldprice:" ",
+        sale:"",
+        line:"",
+        icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOoPLBr6dN2ha1EVjelvUgn5UdGzVcUbwqkA&usqp=CAU",
+        article1:"Article",
+    article2:"000123",
+    country1:"Country",
+    country2:"Armenia",
+    region1:"Region",
+    region2:"Armenia",
+     manufacturer1:"Manufacturer",
+    manufacturer2:"Areni",
+    color:"color red",
+    type:"Type dry",
+    Volume:"Valume 750ml",
+    Alkohol:"Alcohol 55%",
+    price:"6,75",
+    Description:"Description",
+    text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a venenatis ex. Praesent ut diam sit amet ipsum hendrerit lacinia nec ut turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed sollicitudin lacus sed quam laoreet, et elementum diam varius. Etiam eu placerat libero.Suspendisse a venenatis ex. Praesent ut diam sit amet ipsum hendrerit lacinia nec ut turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed sollicitudin lacus sed quam laoreet, et elementum diam varius. Etiam eu placerat libero.  ",
+    
+    },
+    {
+        name:"“Tushpa” white wine", 
+        persent: "Alc 55% 0.75l",
+        url: "wino7.png",
+       oldprice:"9,55 ",
+        sale:"10%",
+        icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOoPLBr6dN2ha1EVjelvUgn5UdGzVcUbwqkA&usqp=CAU",
+        article1:"Article",
+    article2:"000123",
+    country1:"Country",
+    country2:"Armenia",
+    region1:"Region",
+    region2:"Armenia",
+     manufacturer1:"Manufacturer",
+    manufacturer2:"Areni",
+    color:"color red",
+    type:"Type dry",
+    Volume:"Valume 750ml",
+    Alkohol:"Alcohol 55%",
+    price:"7,59",
+    Description:"Description",
+    text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a venenatis ex. Praesent ut diam sit amet ipsum hendrerit lacinia nec ut turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed sollicitudin lacus sed quam laoreet, et elementum diam varius. Etiam eu placerat libero.Suspendisse a venenatis ex. Praesent ut diam sit amet ipsum hendrerit lacinia nec ut turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed sollicitudin lacus sed quam laoreet, et elementum diam varius. Etiam eu placerat libero.  ",
+    
+    },
+    {
+    name:"“Areni” Qoto red wine", 
+    persent: "Alc 55% 0.75l",
+    url: "wino8.png",
+    oldprice:"9,55 ",
+    sale:"10%",
+    icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOoPLBr6dN2ha1EVjelvUgn5UdGzVcUbwqkA&usqp=CAU",
+    article1:"Article",
+    article2:"000123",
+    country1:"Country",
+    country2:"Armenia",
+    region1:"Region",
+    region2:"Armenia",
+     manufacturer1:"Manufacturer",
+    manufacturer2:"Areni",
+    color:"color red",
+    type:"Type dry",
+    Volume:"Valume 750ml",
+    Alkohol:"Alcohol 55%",
+    price:"7,99",
+    Description:"Description",
+    text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a venenatis ex. Praesent ut diam sit amet ipsum hendrerit lacinia nec ut turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed sollicitudin lacus sed quam laoreet, et elementum diam varius. Etiam eu placerat libero.Suspendisse a venenatis ex. Praesent ut diam sit amet ipsum hendrerit lacinia nec ut turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed sollicitudin lacus sed quam laoreet, et elementum diam varius. Etiam eu placerat libero.  ",
+    
+    },
+    {
+    name:"“Karas” red wine", 
+    persent: "Alc 55% 0.75l",
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQen4f2T6czenr1OnfEvvYhn2dktwOxl6VYd9ap3rwn463tSFouuSs-ZEp0kIUK-R7fcmI&usqp=CAU",
+    oldprice:"9,55 ",
+    sale:"10%",
+     icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOoPLBr6dN2ha1EVjelvUgn5UdGzVcUbwqkA&usqp=CAU",
+        article1:"Article",
+    article2:"000123",
+    country1:"Country",
+    country2:"Armenia",
+    region1:"Region",
+    region2:"Armenia",
+     manufacturer1:"Manufacturer",
+    manufacturer2:"Areni",
+    color:"color red",
+    type:"Type dry",
+    Volume:"Valume 750ml",
+    Alkohol:"Alcohol 55%",
+    price:"5,99",
+    Description:"Description",
+    text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a venenatis ex. Praesent ut diam sit amet ipsum hendrerit lacinia nec ut turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed sollicitudin lacus sed quam laoreet, et elementum diam varius. Etiam eu placerat libero.Suspendisse a venenatis ex. Praesent ut diam sit amet ipsum hendrerit lacinia nec ut turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed sollicitudin lacus sed quam laoreet, et elementum diam varius. Etiam eu placerat libero.  ",
+   
+    }
+    
+]
+let x=0
+   za.innerHTML=x
+for(i=0;i<arr.length;i++){
+    let container2=document.createElement('div')
+    container2_item2.appendChild(container2)
+    container2.style.width="250px"
+    container2.style.height="420px"
+    container2.style.border="1px solid #E8E7E5"
+    container2.style.float="left"
+    container2.setAttribute("class","d1")
+
+    let obj=document.createElement('span')
+    container2.appendChild(obj)
+obj.innerHTML=arr[i].name
+obj.style.width="200px"
+obj.style.height="30px"
+obj.style.fontFamily='Gabriola'
+obj.style.color="#A40D30"
+obj.style.fontSize="24px"
+obj.style.lineHeight="135%"
+obj.style.fontWeight="bold"
+
+let obj1=document.createElement('span')
+    container2.appendChild(obj1)
+obj1.innerHTML=arr[i].persent
+obj1.style.width="100px"
+obj1.style.height="50px"
+obj1.style.fontFamily='Futura Bk BT'
+obj1.style.color="grey"
+obj1.style.fontSize="14px"
+obj1.style.lineHeight="135%"
+obj1.style.fontWeight="bold"
+
+let obj2=document.createElement('img')
+    container2.appendChild(obj2)
+    obj2.style.width="110px"
+    obj2.style.height="215px"
+obj2.src=arr[i].url
+
+let zvezda=document.createElement('div')
+container2.appendChild(zvezda)
+zvezda.style.background="url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTD5aPYa7QVFJqsTLll2wrmmSWVUgeMOWtAu5MSqFYZW7rP4iY4hN_7LxY7M6McP-3UVw&usqp=CAU')"
+zvezda.style.backgroundSize="cover"
+zvezda.style.width="150px"
+zvezda.style.height="50px"
+
+
+let line=document.createElement('div')
+    container2.appendChild(line)
+    line.style.width="200px"
+    line.style.borderBottom="1px solid #e9e3e3"
+  
+
+let dd=document.createElement('div')
+    container2.appendChild(dd)
+   dd.style.width="100px"
+    dd.style.height="30px"
+    dd.style.marginRight="100px"
+let obj4=document.createElement('span')
+   dd.appendChild(obj4)
+   obj4.innerHTML=arr[i].oldprice
+obj4.style.height="30px"
+obj4.style.fontSize="15px"
+obj4.style.color="grey"
+obj4.style.textDecoration="line-through"
+let obj6=document.createElement('span')
+dd.appendChild(obj6)
+obj6.innerHTML=arr[i].sale
+obj6.style.width="50px"
+obj6.style.fontSize="15px"
+obj6.style.background="#A40D30"
+ 
+let obj5=document.createElement('span')
+container2.appendChild(obj5)
+obj5.innerHTML=arr[i].price+"$"
+obj5.style.width="200px"
+obj5.style.height="30px"
+obj5.style.fontSize="20px"
+
+//*create enq arel zambyux u zambyuxi vra cliciq verevi zambyuxum avelanum e
+let icondiv=document.createElement('div')
+icondiv.style.width="40px"
+icondiv.style.height="40px"
+icondiv.style.marginLeft="190px"
+container2.appendChild(icondiv)
+let icon=document.createElement('img')
+icon.style.width="30px"
+icon.style.height="30px"
+icondiv.appendChild(icon)
+icon.src=arr[i].icon
+icon.onclick=function(){
+    x++
+za.innerHTML=x
+}
+}
+
+
+for(j=0;j<arr2.length;j++){
+    let container3=document.createElement('div')
+    container3_item2.appendChild(container3)
+    container3.style.width="250px"
+    container3.style.height="420px"
+    container3.style.border="1px solid #E8E7E5"
+    container3.style.float="left"
+    container3.setAttribute("class","d2")
+
+    let name=document.createElement('span')
+    container3.appendChild(name)
+name.innerHTML=arr2[j].name
+name.style.width="200px"
+name.style.height="30px"
+name.style.fontFamily='Gabriola'
+name.style.color="#A40D30"
+name.style.fontSize="24px"
+name.style.lineHeight="135%"
+name.style.fontWeight="bold"
+
+let persent=document.createElement('span')
+    container3.appendChild(persent)
+persent.innerHTML=arr2[i].persent
+persent.style.width="100px"
+persent.style.height="50px"
+persent.style.fontFamily='Futura Bk BT'
+persent.style.color="grey"
+persent.style.fontSize="14px"
+persent.style.lineHeight="135%"
+persent.style.fontWeight="bold"
+
+let img=document.createElement('img')
+    container3.appendChild(img)
+img.src=arr2[j].url
+
+let zvezda=document.createElement('div')
+container3.appendChild(zvezda)
+zvezda.style.background="url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTD5aPYa7QVFJqsTLll2wrmmSWVUgeMOWtAu5MSqFYZW7rP4iY4hN_7LxY7M6McP-3UVw&usqp=CAU')"
+zvezda.style.backgroundSize="cover"
+zvezda.style.width="150px"
+zvezda.style.height="60px"
+
+
+let line2=document.createElement('div')
+    container3.appendChild(line2)
+    line2.style.width="200px"
+    line2.style.borderBottom="1px solid #e9e3e3"
+  
+
+let dd2=document.createElement('div')
+    container3.appendChild(dd2)
+   dd2.style.width="100px"
+    dd2.style.height="30px"
+    dd2.style.marginRight="100px"
+let oldprice=document.createElement('span')
+   dd2.appendChild(oldprice)
+   oldprice.innerHTML=arr2[j].oldprice
+oldprice.style.height="30px"
+oldprice.style.fontSize="15px"
+oldprice.style.color="grey"
+oldprice.style.textDecoration="line-through"
+let sale=document.createElement('span')
+dd2.appendChild(sale)
+sale.innerHTML=arr2[j].sale
+sale.style.width="50px"
+sale.style.fontSize="15px"
+sale.style.background="#A40D30"
+
+let price=document.createElement('span')
+container3.appendChild(price)
+price.innerHTML=arr2[j].price+"$"
+price.style.width="200px"
+price.style.height="30px"
+price.style.fontSize="20px"
+
+
+//*create enq arel zambyux u zambyuxi vra cliciq verevi zambyuxum avelanum e
+let icondiv=document.createElement('div')
+icondiv.style.width="40px"
+icondiv.style.height="40px"
+icondiv.style.marginLeft="190px"
+container3.appendChild(icondiv)
+let icon=document.createElement('img')
+icon.style.width="30px"
+icon.style.height="30px"
+icondiv.appendChild(icon)
+icon.src=arr2[i].icon
+icon.onclick=function(){
+    x++
+za.innerHTML=x
+}
+
+}
+
+
+
+//*3-rd nkarneri zangvac +slider*
+ let arr3=["slid1.png","slid2.png ","slid3.png","slid4.png","slide5.png","slid6.png"]
+
+
+for(let f=0; f<arr3.length;f++){
+    let container=document.createElement('img')
+ container.style.width="100px"
+container.style.height="70px"
+container.style.float="left"
+container7_item2.appendChild(container)
+container.src=arr3[f]
+}
+
+
+//more...???
+let text="Armenia wine";
+let btn=document.getElementsByClassName('btn');
+let p1=document.getElementsByClassName('p1');
+
+
+for(let h=0; h<p1.length; h++){
+   function f1(a){
+    a.onclick=function(){
+if(p1[h].innerHTML==""){
+            p1[h].innerHTML=text
+             
+        }
+        else{
+            p1[h].innerHTML=" "
+              
+        }
+    }
+
+}
+}
+
+let mainListDiv = document.getElementById("mainListDiv"),
+    mediaButton = document.getElementById("mediaButton");
+
+mediaButton.onclick = function () {
+    
+    "use strict";
+    
+    mainListDiv.classList.toggle("show_list");
+    mediaButton.classList.toggle("active");
+};
+
